9a1d87ad44655cec8aa593b71be49184de0a92e0
Peddi reddy Bhumireddy <113705525+smatwoks@users.noreply.github.com>
2025-05-07 11:00:31 +0530

    Update page.tsx

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -2,14 +2,6 @@
 
 export default async function Home() {
  
-    <div className="grid grid-cols-3 gap-8">
-      <div className="col-span-1">
-      </div>
-      <div className="col-span-2">
-        <div className="col-span-2 flex flex-col gap-6">
-  hello hi
-        </div>
-      </div>
-    </div>
+   <h1>hi</h1>
   );
 }
